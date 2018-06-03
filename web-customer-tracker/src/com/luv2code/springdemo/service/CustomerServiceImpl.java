576b7dba7f498a90cc289d5ecf18735892476ceb
jacob <jacob@jojo>
2018-06-03 15:38:56 +0200

    service annotation

--- web-customer-tracker/src/com/luv2code/springdemo/service/CustomerServiceImpl.java
+++ web-customer-tracker/src/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -3,11 +3,13 @@ package com.luv2code.springdemo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
 	// need to inject customer DAO
